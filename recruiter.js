0e27606c130271033c9ed7cf8572e74bb0253165
Kathryn Rouse <rousekm@wfu.edu>
2017-04-05 15:41:28 -0400

    node and npm do not work on my computer.  Attempted to write index.js (tests) then recruiter.js.  Completely untested even for syntactical errors.

--- recruiter.js
+++ recruiter.js
@@ -8,8 +8,18 @@ var util = require('./util.js');
 //TODO: You need to write this function AND utilize it.
 // bracketFromGPA(decimal GPA);
 function bracketFromGPA(gpa) {
-	// 4-3.5, 3.49 - 3.0, 2.99 - 2.5
-	return; //some form of bracket number
+	if(gpa >= 3.5){
+	return 3;
+	}
+	else if (gpa >= 3.0){
+	return 2;
+	}
+	else if (gpa >= 2.5){
+	return 1;
+	}
+	else{
+	return 0;
+	}
 }
 
 // TODO: recruiter( Array of hireables )
@@ -17,29 +27,51 @@ function recruiter(internArr) {
 
 	// Below is just to help show the syntax you need,
 	// you'll need to process ALL of the hireables like this one and sort
-	var index = 0;
-	var iname = internArr[index].name;
-	var idegr = internArr[index].degree;
-	var igpa = internArr[index].gpa;
-	var iexp = internArr[index].experiance;
+	var iname;
+	var idegr;
+	var igpa;
+	var iexp;
 	var iwage, ivalue, ibracket, imetric;
+	var astrology = [] // keep track of indices of astrology majors
+	var employable = 0 // keep track of employable non-astrology majors
 
-	// Yep, you can use strings as an "index" (technically it's a property) in JavaScript
-	idegr = idegr.toLowerCase();
-	iwage = degreeSWage[idegr];
+	for (i=0; i < internArr.length; i++){
+		iname = internArr[i].name;
+		idegr = internArr[i].degree;
+		igpa = internArr[i].gpa;
+		iexp = internArr[i].experiance;
 
-	// You should use these functions at some point
-	ivalue = util.getValueFromWageAndExp( /*wage, full years of experiance*/ );
-	ibracket = bracketFromGPA ( /*decimal GPA*/ );
-
-	// Hmm... this doesn't seem to follow the spec - fix it
-	imetric = ivalue + ibracket;
-
-	// We really want to add our sorting number "metric" to objects (it really is this easy)
-	internArr[index].metric = imetric;
-
+		// Yep, you can use strings as an "index" (technically it's a property) in JavaScript
+		idegr = idegr.toLowerCase();
+		iwage = degreeSWage[idegr];
+		if (iwage === undefined){
+			imetric = 0;
+		}
+		} else {
+			// You should use these functions at some point
+			if (idegr === "astrology"){
+				// ensure astrology major's value to the company is lower than everybody else
+				ivalue = util.getValueFromWageAndExp(1,Math.floor(iexp));
+				ibracket = bracketFromGPA(igpa);
+				imetric = ivalue + .05*ibracket;
+				employable++;
+			} else {
+				ivalue = util.getValueFromWageAndExp(iwage, Math.floor(iexp));
+				ibracket = bracketFromGPA(igpa);
+				//ivalue increases by .2
+				if (ibracket != 0){
+					imetric = ivalue + .05*ibracket;
+					employable++;
+				} else {
+					imetric = 0;
+				}
+			}
+		}
+		// We really want to add our sorting number "metric" to objects (it really is this easy)
+		internArr[i].metric = imetric;
+	}
 	// and then sort them all (it doesn't return anything, it modifies the array sent)
-	util.sortInternObjects( /*Array of hireables with "metric" as a property*/ );
+	util.sortInternObjects(internArr);
 
 
 	// Output 
@@ -49,7 +81,13 @@ function recruiter(internArr) {
 	// and people earlier in the array have equal or greater values for "metric" than
 	// people further down.
 
-	return internArr;
+	// if employable === 0, we only want the interns with astrology degrees
+	if (employable === 0){
+		var Empty = [];
+		return Empty;
+	} else {
+		return internArr.slice(0,employable);
+	}
 };
 
 module.exports = {
--- test/index.js
+++ test/index.js
@@ -31,7 +31,7 @@ test('util.sortInternObjects', function(t) {
 	inputArr[3].metric = 0;
 
 	// Lets get the input sorted manually, in the expected array
-	var expectedArr = inputArr.slice();
+	var expectedArr = inputArr.slice(); //copying array in new location
 	expectedArr = [
 		expectedArr[0], // 3
 		expectedArr[2], // 2
@@ -66,7 +66,183 @@ test('util.sortInternObjects', function(t) {
 });
 
 // Your tests go here  (methods reference: https://www.npmjs.com/package/tape#testname-opts-cb )
+test('bracketFromGPA', function(t) {
+	t.deepEqual(recruiter.bracketFromGPA(3.5),3,"returns bracket three");
+	t.deepEqual(recruiter.bracketFromGPA(3.4),2,"returns bracket two");
+	t.deepEqual(recruiter.bracketFromGPA(2.99),1,"returns bracket 1");
+	t.deepEqual(recruiter.bracketFromGPA(2.49),0,"returns bracket 0");
 
+	t.end();
+});
+
+test('recruiterFunction', function(t) {
+	var collArr = [interns[0], interns[1], interns[2], interns[3]];
+
+	var inputArr = collArr.slice();
+	//Create Lists of interns that have the properties we want to test
+	// List 1: Check that if all value to company are different, GPA is ignored
+	inputArr[0].degree = "petroleum engineering";
+	inputArr[1].degree = "petroleum engineering";
+	inputArr[2].degree = "computer science & engineering";
+	inputArr[3].degree = "physician assistant studies";
+
+	inputArr[0].gpa = 4.0;
+	inputArr[1].gpa = 2.7;
+	inputArr[2].gpa = 3.5;
+	inputArr[3].gpa = 2.9;
+
+	inputArr[0].experiance = 0;
+	inputArr[1].experiance = 3; // uses experiance to change value to company instead of degree
+	inputArr[2].experiance = 0;
+	inputArr[3].experiance = 0;
+
+	//Copy list of interns and sort it manually
+	var expectedArr = inputArr.slice(); //copying array in new location
+	expectedArr = [
+		expectedArr[1], // 
+		expectedArr[0], // 
+		expectedArr[3], // 
+		expectedArr[2]  // 
+	];
+
+	// Lets make a copy of the input to sort with the function 
+	var actualArr = inputArr.slice();
+
+	// do something (in-place)
+	actualArr = recruiter.recruiter(actualArr);
+
+  t.deepEqual(actualArr, expectedArr, 'check: ignores GPA if wages all different, checks that experiance affects wage');
+
+	// List 2: Check that if value to company are same, GPA is tie breaker
+	// checking in 2 ways, same degree & diff degree with same wage
+	// note: there is no tie breaker post GPA, so they will appear in the same order
+	inputArr[0].degree = "mining engineering";
+	inputArr[1].degree = "petroleum engineering";
+	inputArr[2].degree = "petroleum engineering";
+	inputArr[3].degree = "chemical engineering";
+
+	inputArr[0].gpa = 4.0;
+	inputArr[1].gpa = 2.7;
+	inputArr[2].gpa = 3.1;
+	inputArr[3].gpa = 3.5;
+
+	inputArr[0].experiance = 0;
+	inputArr[1].experiance = 2;
+	inputArr[2].experiance = 2;
+	inputArr[3].experiance = 0;
+
+	//Copy list of interns and sort it manually
+	var expectedArr = inputArr.slice(); //copying array in new location
+	expectedArr = [
+		expectedArr[2], // 
+		expectedArr[1], // 
+		expectedArr[3], // 
+		expectedArr[0]  // 
+	];
+
+	// Lets make a copy of the input to sort with the function 
+	var actualArr = inputArr.slice();
+
+	// do something (in-place)
+	actualArr = recruiter.recruiter(actualArr);
+
+  t.deepEqual(actualArr, expectedArr, 'uses GPA to tie break when wages are the same, checks for multiple causes of same wage');
+
+	// List 3: Check that exceptions - GPA < 2.5, unknown degrees, astrology
+	collArr = [interns[0], interns[1], interns[2], interns[3], interns[4]];
+	inputArr = collArr.slice()
+	inputArr[0].degree = "ugg";
+	inputArr[1].degree = "petroleum engineering";
+	inputArr[2].degree = "astrology";
+	inputArr[3].degree = "astrology";
+	inputArr[4].degree = "astrology";
+
+	inputArr[2].experiance = 1;
+	inputArr[4].experiance = 1;
+	inputArr[3].experiance = 5;
+
+	inputArr[0].gpa = 4.0;
+	inputArr[1].gpa = 2.49;
+	inputArr[2].gpa = 3.1;
+	inputArr[3].gpa = 1.5;
+	inputArr[4].gpa = 2.3;
+
+	//Copy list of interns and sort it manually
+	var expectedArr = inputArr.slice(); //copying array in new location
+	expectedArr = [
+		expectedArr[4],
+		expectedArr[2],
+		expectedArr[3] // 
+	];
+
+	// Lets make a copy of the input to sort with the function 
+	var actualArr = inputArr.slice();
+
+	// do something (in-place)
+	recruiter.recruiter(actualArr);
+
+  t.deepEqual(actualArr, expectedArr, 'returns astrology when all candidates are bad, removes low GPA, removes unknown degree');
+
+	// List 4: Check that astrology loses to any qualified candidate
+	inputArr[0].degree = "child development";
+	inputArr[1].degree = "boots";
+	inputArr[2].degree = "pastoral ministry";
+	inputArr[3].degree = "youth ministry";
+	inputArr[4].degree = "astrology";
+
+	inputArr[0].experiance = 0;
+	inputArr[2].experiance = 0;
+	inputArr[3].experiance = 0;
+	inputArr[4].experiance = 10;
+
+	inputArr[0].gpa = 2.51;
+	inputArr[1].gpa = 3.2;
+	inputArr[2].gpa = 2.50;
+	inputArr[3].gpa = 3.1;
+	inputArr[4].gpa = 4.0;
+
+	//Copy list of interns and sort it manually
+	var expectedArr = inputArr.slice(); //copying array in new location
+	expectedArr = [
+		expectedArr[3],
+		expectedArr[2],
+		expectedArr[0],
+		expectedArr[4]
+	];
+
+	// Lets make a copy of the input to sort with the function 
+	var actualArr = inputArr.slice();
+
+	// do something (in-place)
+	recruiter.recruiter(actualArr);
+
+  t.deepEqual(actualArr, expectedArr, 'returns astrology only after good candidates');
+
+	// List 5: Check program will return an empty list
+	inputArr[0].degree = "";
+	inputArr[1].degree = "petroleum engineering";
+	inputArr[2].degree = "gobbledy gook";
+	inputArr[3].degree = "computer science";
+
+	inputArr[0].gpa = 4.0;
+	inputArr[1].gpa = 2.49;
+	inputArr[2].gpa = 3.1;
+	inputArr[3].gpa = 1.0;
+
+	//Copy list of interns and sort it manually
+	var expectedArr = inputArr.slice(); //copying array in new location
+	expectedArr = [];
+
+	// Lets make a copy of the input to sort with the function 
+	var actualArr = inputArr.slice();
+
+	// do something (in-place)
+	recruiter.recruiter(actualArr);
+
+  t.deepEqual(actualArr, expectedArr, 'will return empty list when no qualified candidates');
+
+	t.end();
+});
 // test('Test Name', function(t) {
 
 //   if (/*some condition*/) {
